5687576789b487665a70b8953e3d597d6613a6b6
alaa <a99750579@gmail.com>
2026-04-24 22:15:20 +0300

    fix buttons SEO

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,4 @@
-import dynamic from "next/dynamic";
-
-const KhidmaAIV2 = dynamic(() => import("./components/KhidmaAIV2"));
+import KhidmaAIV2 from "./components/KhidmaAIV2";
 
 export default function Page() {
   return <KhidmaAIV2 />;
